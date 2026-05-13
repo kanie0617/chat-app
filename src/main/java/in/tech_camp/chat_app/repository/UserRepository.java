5dda4fa200ca8192ea57b08864a3df512f5b139e
kanie0617 <haruna.kanie@smart-group.co.jp>
2026-05-13 16:18:56 +0900

    ユーザー編集機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import in.tech_camp.chat_app.entity.UserEntity;
 
@@ -16,4 +17,12 @@ public interface UserRepository {
   //UserAuthenticationServiceで使用するemailでユーザー情報を取得するメソッドを定義
   @Select("SELECT * FROM users WHERE email = #{email}")
   UserEntity findByEmail(String email);
+
+  //idでユーザー情報を取得するメソッドを定義
+  @Select("SELECT * FROM users WHERE id = #{id}")
+  UserEntity findById(Integer id);
+
+  //DBに保存されているユーザーの情報を更新
+  @Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
+  void update(UserEntity user);
 }
