b8b1cec19325bc9dd4f35cecc500a18e5a3ad91c
kanie0617 <haruna.kanie@smart-group.co.jp>
2026-05-15 09:32:22 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomRepository.java
@@ -1,8 +1,15 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.RoomEntity;
 
 @Mapper
 public interface RoomRepository {
-
+  //roomsテーブルにRoomEntityの情報を追加するメソッド
+  @Insert("INSERT INTO rooms(name) VALUES(#{name})")
+  @Options(useGeneratedKeys=true, keyProperty="id")
+  void insert(RoomEntity roomEntity);
 }
--- src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/RoomUserRepository.java
@@ -1,8 +1,15 @@
 package in.tech_camp.chat_app.repository;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+import in.tech_camp.chat_app.entity.RoomUserEntity;
 
 @Mapper
 public interface RoomUserRepository {
-
+//RoomUserEntityの情報を受け取り、room_usersテーブルに保存するメソッド
+@Insert("INSERT INTO room_users(user_id, room_id) VALUES(#{user.id}, #{room.id})")
+@Options(useGeneratedKeys=true, keyProperty="id")
+void insert(RoomUserEntity roomUserEntity);
 }
