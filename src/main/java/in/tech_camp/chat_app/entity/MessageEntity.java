d96c5dd3b43afecea67a499f6cd8788f5c6a1d6e
kanie0617 <haruna.kanie@smart-group.co.jp>
2026-05-15 14:29:12 +0900

    messagesテーブルの作成

---
+++ src/main/java/in/tech_camp/chat_app/entity/MessageEntity.java
@@ -0,0 +1,17 @@
+package in.tech_camp.chat_app.entity;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MessageEntity {
+  private Integer id;
+  private String content;
+  private String image;
+  private Timestamp createdAt;
+
+  //外部キー
+  private UserEntity user;
+  private RoomEntity room;
+}
