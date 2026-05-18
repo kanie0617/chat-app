d64629d6f8c59b55dd2346089015815a8af65b4e
kanie0617 <haruna.kanie@smart-group.co.jp>
2026-05-18 14:08:11 +0900

    画像保存の実装

--- src/main/java/in/tech_camp/chat_app/form/MessageForm.java
+++ src/main/java/in/tech_camp/chat_app/form/MessageForm.java
@@ -1,5 +1,7 @@
 package in.tech_camp.chat_app.form;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import in.tech_camp.chat_app.validation.ValidationPriority1;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -9,4 +11,5 @@ public class MessageForm {
   @NotBlank(message = "Message can't be blank", groups = ValidationPriority1.class)
   private String content;
   
+  private MultipartFile image;
 }
